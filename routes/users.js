136b258fb6fcc5d852854c27bf381cb1386e46fc
Prathamesh Tambe <tambe.p@husky.neu.edu>
2020-10-12 18:19:45 -0400

    sorted users and classes function for different files and removed unnecessory variables declared in users.js which will be using unncessaory spaces

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const databaseAccess = require("./databaseAccess");
+
+//router.get("/listAllUsers", processAllUsers);  // dont need this Note - while creating Users api by default /users will return list of users 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', processAllUsers);               
+
+function createLastNameLookup(array) {
+  const returnValue = {};
+  for (let i = 0; i < array.length; i++) {
+    returnValue[array[i].id] = array[i].last;
+  }
+  return returnValue;
+}
+
+// removed unnecessory varibles which will be using space
+async function processAllUsers(req, res) {
+  let stuff = await databaseAccess.getUsers();          
+  let lastNames = await databaseAccess.getLastNames();
+  const lastNameLookup = createLastNameLookup(lastNames);
+  for (let i = 0; i < stuff.length; i++) {
+    stuff[i].last = lastNameLookup[stuff[i].id];;
+  }
+  res.json(stuff);
+}
 
 module.exports = router;
