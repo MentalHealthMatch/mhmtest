1585c5027d1f79675efbae4197c50d7b917f4b8c
Zephyr <flyingarmadillo@gmail.com>
2020-10-21 22:33:24 -0700

    Test suite now tests the two main endpoints

--- test/test.js
+++ test/test.js
@@ -1,7 +1,30 @@
 const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const should = chai.should();
+chai.use(chaiHttp);
 
-describe("Tests run at all", () => {
-  it("1=1", () => {
-    assert.equal(1, 1);
-  })
-})
+describe("GET /listAllUsers", function() {
+  it("should fetch all users", function(done) {
+    chai.request(app)
+      .get("/listAllUsers")
+      .end((err, result) => {
+        result.should.have.status(200);
+        assert.equal(result.body.length, 2);
+        done();
+      });
+  });
+});
+
+describe("GET /listallclasses", function() {
+  it("should fetch all classes", function(done) {
+    chai.request(app)
+      .get("/listallclasses")
+      .end((err, result) => {
+        result.should.have.status(200);
+        assert.equal(result.body.length, 2);
+        done();
+      });
+  });
+});
