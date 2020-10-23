69b3c3a2333ba8f885041afb8fe9b75dea20e1d7
Zephyr <flyingarmadillo@gmail.com>
2020-10-23 13:04:39 -0700

    Minimal changes to the index view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Zephyr Dear's mhmtest solution" });
 });
 
 module.exports = router;
