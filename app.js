45da8aa2ae7d01b4589e0c5be2b97aa74161f760
B <B@%20>
2020-10-28 14:47:22 -0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -29,13 +31,10 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.error (err);
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.end ();
 });
 
 module.exports = app;
