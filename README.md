2e6b8fa9e8d96c2eacd6bdfd90925250116cce5c
Shazia Adeel <shaziaadeel@Shazias-MacBook-Pro-2.local>
2020-11-06 16:21:13 -0800

    Fixing issues in listAllClasses, refactory listAllUsers and add API calls for getting single user and class

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
 # mhmtest
 
-A simple coding test in project form
+Changes Done:
 
-This test is intended to test your basic facility with a Node JS Backend. As such, we're not going to explain much. We'll just say this:
+- Refactor code of listAllUsers
+- Add new APIs for geting single user or class
+- Fix issue in listAllClasses call
 
-Stylistically this is about as bad as we could do on short notice. You are free to fix any things that really annoy you, but those aren't core tasks.
+Application support following APIs
 
-Consider fixing bad name choices.  Feel free to rename methods and even URL paths to things that make sense to you.  There is no right or wrong, but
-we'd like to get a sense of your style.  The structure is lame too.  If you want to move things to different files or folders, go nuts.
-
-There are two core tasks:
-
-1). the code that services "/listAllUsers" is awful and needs to be refactored
-
-2). "/listallclasses" is broken and needs to be fixed.
-
-Beyond this, you're free to add any functionality you think would be interesting for us to see and to get a sense of how you approach coding and problem solving.
-
-When you're done, create a pull request and we'll take a look!
+- /listAllUsers : For getting all users from database
+- /getUser?userid=1 : For getting user of given id from database
+- /listAllClasses : For getting all classes from database
+- /getClass?classid=2 : For getting class of given id from database
--- routes/index.js
+++ routes/index.js
@@ -8,25 +8,37 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/listAllUsers", processAllUsers);
-
-router.get("/listallclasses", listallclasses);
+//// For getting all user from database
+router.get("/listAllUsers", listAllUsers);
+/// For getting all classes from database
+router.get("/listAllClasses", listAllClasses);
+// Get user for given id
+router.get("/getUser", getUser);
+// Get class for given id
+router.get("/getClass", getClass);
 
 module.exports = router;
 
-async function processAllUsers(req, res) {
-  let stuff = await database.getUsers();
-  for (let i = 0; i < stuff.length; i++) {
-    const next = stuff[i];
-    let lastNames = await database.getLastNames();
-    const lastNameLookup = createLastNameLookup(lastNames);
-    const nextId = next.id;
-    const last = lastNameLookup[nextId];
-    next.last = last;
-  }
-  res.json(stuff);
-}
+////////////////////////////////////////////////////////////////
+///Function return json format out containing
+/// name: {firstname}
+/// last: {lastName}
+/// id: {userId}
+////////////////////////////////////////////////////////////////
+async function listAllUsers(req, res) {
+  let users = await database.getUsers();
+  let lastNames = await database.getLastNames();
+  const lastNameLookup = createLastNameLookup(lastNames);
 
+  for (let i = 0; i < users.length; i++) {
+    users[i].last = lastNameLookup[users[i].id];
+  }
+  res.json(users);
+}
+//////////////////////////////////////////////////////////////
+/// Function return a map, each item contain
+/// userId, lastName
+//////////////////////////////////////////////////////////////
 function createLastNameLookup(array) {
   const returnValue = {};
   for (let i = 0; i < array.length; i++) {
@@ -36,6 +48,68 @@ function createLastNameLookup(array) {
   return returnValue;
 }
 
-async function listallclasses(req, res) {
-  res.json(databaseAccess.getAllClasses());
+//////////////////////////////////////////////////////////////
+/// Function return json containing all classes from database
+//////////////////////////////////////////////////////////////
+async function listAllClasses(req, res) {
+  let classes = await databaseAccess.getAllClasses();
+  res.json(classes);
+}
+
+//////////////////////////////////////////////////////////////
+// function for getting single user
+//////////////////////////////////////////////////////////////
+async function getUser(req, res) {
+  console.log(req.query.userid);
+  if (req.query.userid == undefined) {
+    res.send("error: user id not found");
+    return;
+  }
+
+  let users = await database.getUsers();
+  let lastNames = await database.getLastNames();
+  let found = false;
+  for (let i = 0; i < users.length; i++) {
+    if (req.query.userid == users[i].id) {
+      let user = users[i];
+
+      for (let j = 0; j < lastNames.length; j++) {
+        if (lastNames[j].id == users[i].id) {
+          user.last = lastNames[j].last;
+          console.log("user=" + user);
+          res.json(user);
+          found = true;
+          break;
+        }
+      }
+    }
+  }
+
+  if (found == false)
+    res.send("error: User of id " + req.query.userid + " not found ");
+}
+
+//////////////////////////////////////////////////////////////
+/// Function return class of given id
+//////////////////////////////////////////////////////////////
+async function getClass(req, res) {
+  console.log(req.query.classid);
+  if (req.query.classid == undefined) {
+    res.send("error: class id not found");
+    return;
+  }
+
+  let classes = await databaseAccess.getAllClasses();
+  let found = false;
+
+  for (let i = 0; i < classes.length; i++) {
+    if (req.query.classid == classes[i].id) {
+      res.json(classes[i]);
+      found = true;
+      break;
+    }
+  }
+
+  if (found == false)
+    res.send("error: Class of id " + req.query.classid + " not found ");
 }
