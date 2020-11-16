c5648cd1cd7bd4bdc6e67e23b82b79478b2e1ec2
developstuff93 <developstuff93@gmail.com>
2020-11-16 00:06:35 -0500

    add error handlers on controllers

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -1,9 +1,14 @@
+const { NotExtended } = require("http-errors");
 const db = require("../models");
 const Class = db.Class;
 
 module.exports = {
-  list: async (req, res) => {
-    const classes = await Class.getAllClasses();
-    res.json(classes);
+  list: async (req, res, next) => {
+    try {
+      const classes = await Class.getAllClasses();
+      res.json(classes);
+    } catch (error) {
+      next(error);
+    }
   },
 };
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,13 +2,17 @@ const db = require("../models");
 const User = db.User;
 
 module.exports = {
-  list: async (req, res) => {
-    const users = await User.getUsers();
-    const lastNames = await User.getLastNames();
-    const stuff = users.map((user) => ({
-      ...user,
-      last: lastNames.find((lastNameObject) => lastNameObject.id === user.id).last,
-    }));
-    res.json(stuff);
+  list: async (req, res, next) => {
+    try {
+      const users = await User.getUsers();
+      const lastNames = await User.getLastNames();
+      const stuff = users.map((user) => ({
+        ...user,
+        last: lastNames.find((lastNameObject) => lastNameObject.id === user.id).last,
+      }));
+      res.json(stuff);
+    } catch (error) {
+      next(error);
+    }
   },
 };
