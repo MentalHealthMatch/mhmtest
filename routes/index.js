7a66dbb4ec5ec82346e2c7b9b0a2efe080f288a5
developstuff93 <developstuff93@gmail.com>
2020-11-16 00:02:49 -0500

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,9 @@
-var express = require("express");
-const databaseAccess = require("./databaseAccess");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userRoute = require("./user.route");
+const classRoute = require("./class.route");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-router.get("/listAllUsers", processAllUsers);
-
-router.get("/listallclasses", listallclasses);
+router.use("/user", userRoute);
+router.use("/class", classRoute);
 
 module.exports = router;
-
-async function processAllUsers(req, res) {
-  const users = await databaseAccess.getUsers();
-  const lastNames = await databaseAccess.getLastNames();
-  const stuff = users.map((user) => ({
-    ...user,
-    last: lastNames.find((lastNameObject) => lastNameObject.id === user.id).last,
-  }));
-  res.json(stuff);
-}
-
-async function listallclasses(req, res) {
-  const classes = await databaseAccess.getAllClasses();
-  res.json(classes);
-}
